8d66fc0b5d7a2febe187bee7c3995c2fe560b99e
Elliott Grieco <elliott.grieco@nyu.edu>
2018-06-25 11:23:19 -0400

    Starts controller testing

--- karma.conf.js
+++ karma.conf.js
@@ -5,12 +5,7 @@ module.exports = (config) => {
     browsers: ['PhantomJS'],
     files: [
       'node_modules/angular/angular.js',
-      // 'node_modules/angular-material/angular-material.js',
-      // 'node_modules/angular-messages/angular-messages.js',
-      // 'node_modules/angular-animate/angular-animate.js',
-      // 'node_modules/angular-aria/angular-aria.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      // 'node_modules/angular-material/angular-material-mocks.js',
       'dist/module.js',
       'test/**/*.spec.js',
     ],
@@ -20,16 +15,5 @@ module.exports = (config) => {
         'sourcemap',
       ],
     },
-  //   webpack: {
-  //     mode: 'development',
-  //     module: {
-  //       rules: [
-  //         {
-  //           loader: 'babel-loader',
-  //         },
-  //       ],
-  //     },
-  //     devtool: 'inline-source-map',
-  //   },
   })
 }
